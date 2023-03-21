bddc521dd6a2bbe8ecaaff01176fa69a75cfbf0c
loren <lorenmichaelh@gmail.com>
2023-03-21 17:16:58 -0500

    commit tree 3e25abc274534099c8fbbbe22817e98f54ccbf05

--- lib/linked_list.rb
+++ lib/linked_list.rb
@@ -2,4 +2,28 @@ require_relative './node'
 
 class LinkedList
 
+  def prepend
+
+  end
+
+  def append
+
+  end
+
+  def delete_head
+
+  end
+
+  def delete_tail
+
+  end
+
+  def add_after index, node
+
+  end
+
+  def search value
+    
+  end
+
 end
